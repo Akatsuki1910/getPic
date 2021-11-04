4c9f01dd97c874a0dbf2d82b74e7b851b844a2b5
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2021-11-04 13:58:14 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
-  div {{data}}
+  div
+    div {{nowCount}}/{{allLength}}
+    div
+      img(v-for="(pic,i) in data" :key="i" :src="pic")
 </template>
 
 <script lang="ts">
@@ -9,15 +12,44 @@ import { $axios } from '~/utils/api'
 @Component({})
 export default class Index extends Vue {
   data: string[] = []
+  set: Set<string> = new Set()
+  nowCount: number = 0
+  allLength: number = 0
 
   mounted() {
     this.getData('https://qiita.com')
   }
 
   async getData(url: string) {
+    this.set = new Set()
     const d = await $axios.$get('/api/getText', { params: { url } })
-    const t = [...d.matchAll('/http?://[\\w/:%#\\$&\\?\\(\\)~\\.=\\+\\-]+/g')]
-    this.data = t
+    const urlRegexp = /https?:\/\/[^"']+/g
+    const t = [...d.matchAll(urlRegexp)]
+    this.allLength = t.length
+    this.nowCount = 0
+    for (const arr of t) {
+      const url = arr[0]
+      await this.loadImage(url)
+      this.nowCount++
+    }
+
+    this.set.forEach((v) => {
+      this.data.push(v)
+    })
+  }
+
+  async loadImage(src: string) {
+    return await new Promise<void>((resolve) => {
+      const img = new Image()
+      img.onload = () => {
+        this.set.add(src)
+        resolve()
+      }
+      img.onerror = () => {
+        resolve()
+      }
+      img.src = src
+    })
   }
 }
 </script>
