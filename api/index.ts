c90fd5658b792b5040f6a3e277ca5b2ae7c5729e
akatsuki1910 <akatsukicirno@gmail.com>
2021-11-14 16:59:45 +0900

    playwright

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,22 @@
 import express from 'express'
-const chrome = process.env.AWS_LAMBDA_FUNCTION_VERSION
-  ? require('chrome-aws-lambda')
-  : {}
-const puppeteer = require(process.env.AWS_LAMBDA_FUNCTION_VERSION
-  ? 'puppeteer-core'
-  : 'puppeteer')
+const playwright = require(process.env.AWS_LAMBDA_FUNCTION_VERSION
+  ? 'playwright-aws-lambda'
+  : 'playwright-core')
+
+// import playwright from 'playwright-aws-lambda'
 const app: express.Express = express()
 
 app.get('/getText', async (req: express.Request, res: express.Response) => {
-  const browser = await puppeteer.launch({
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless,
-  })
-  const page = await browser.newPage()
+  const browser = process.env.AWS_LAMBDA_FUNCTION_VERSION
+    ? await playwright.launchChromium({
+        channel: 'chrome',
+        headless: true,
+      })
+    : await playwright.chromium.launch({ channel: 'chrome', headless: false })
+  const context = await browser.newContext()
+  const page = await context.newPage()
   const webData = await page.goto(req.query.url as string)
-  const text = await webData.text()
+  const text = await webData!.text()
   await browser.close()
   res.send(text)
 })
