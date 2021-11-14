7ca8ac2da865cfe185142828bca0d5d3a6cdaecb
akatsuki1910 <akatsukicirno@gmail.com>
2021-11-14 11:03:20 +0900

    puppeteer

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,18 @@
 import express from 'express'
-import puppeteer from 'puppeteer'
+const chrome = process.env.AWS_LAMBDA_FUNCTION_VERSION
+  ? require('chrome-aws-lambda')
+  : {}
+const puppeteer = require(process.env.AWS_LAMBDA_FUNCTION_VERSION
+  ? 'puppeteer-core'
+  : 'puppeteer')
 const app: express.Express = express()
 
 app.get('/getText', async (req: express.Request, res: express.Response) => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  })
   const page = await browser.newPage()
   const webData = await page.goto(req.query.url as string)
   const text = await webData.text()
